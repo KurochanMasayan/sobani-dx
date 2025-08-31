10338abe84a3a432cea33167d39830b4bff659d5
HapInS黒谷 <hapins_kurotani@HapInSkurotaninoMacBook-Air.local>
2025-08-31 11:35:54 +0900

    Add .gitignore file
    - node_modules/ と dist/ を除外
    - 個人認証ファイル (.clasprc.json) を除外
    - IDE設定やOS固有ファイルを除外

--- dist/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CALENDAR_ID = void 0;
-exports.CALENDAR_ID = 'c_c89f5fd72defb5ef85547e43d4c019687042f5882de1ec7fdfd2edbb6e2064b4@group.calendar.google.com';
